4a4c8f843a20cfffca5878e09f5bf4302ca4e2ba
ricky ganteng <rionalab@gmail.com>
2023-12-28 22:53:27 +0700

    add antd

---
+++ app/_components/Sidebar/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useState } from "react";
+import { Layout, Menu, Button, theme } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  HomeOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
+
+const { Header, Sider, Content } = Layout;
+
+function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <div className="demo-logo-vertical" />
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        items={[
+          {
+            key: "0",
+            icon: <HomeOutlined />,
+            label: <Link href="/">Home</Link>,
+          },
+          {
+            key: "1",
+            icon: <UserOutlined />,
+            label: <Link href="/reports">Reports</Link>,
+          },
+          {
+            key: "2",
+            icon: <VideoCameraOutlined />,
+            label: <Link href="/settings">Settings</Link>,
+          },
+          {
+            key: "3",
+            icon: <UploadOutlined />,
+            label: "nav 3",
+          },
+        ]}
+      />
+    </Sider>
+  );
+}
+
+export default Sidebar;
