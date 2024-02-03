404e289ea19d074f27d89da75573e16ebd0e6ae7
rionalab <37329349+rionalab@users.noreply.github.com>
2024-02-03 16:48:09 +0700

    remove console

--- app/(pages)/master/inventory/page.tsx
+++ app/(pages)/master/inventory/page.tsx
@@ -5,7 +5,6 @@ import { normalizeTableRow } from "@/helpers";
 
 async function Page() {
   const data = await index();
-  console.log(11, data);
   return <Table rows={normalizeTableRow<any>(data)} />;
 }
 
