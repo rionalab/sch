85b98edc4105c8079a3d7ccfa96f81923b88de8c
rionalab <37329349+rionalab@users.noreply.github.com>
2024-02-04 21:44:55 +0700

    fix error

--- app/(pages)/master/leave/components/form/model.ts
+++ app/(pages)/master/leave/components/form/model.ts
@@ -1,9 +1,7 @@
 import { type Prisma } from "@prisma/client";
 import { type FormFields } from "../../type";
 
-export function modelStore(
-  formValue: FormFields
-): Prisma.PermissionLeaveCreateInput {
+export function modelStore(formValue: FormFields): Prisma.LeaveTypeCreateInput {
   return {
     name: formValue.name,
     description: formValue.description,
