85edb37f20de6b9d3816a8820a19019e53a2ae79
rionalab <37329349+rionalab@users.noreply.github.com>
2024-02-10 14:16:06 +0700

    add code

--- app/(pages)/hrd/permission-leave/components/form/model.ts
+++ app/(pages)/hrd/permission-leave/components/form/model.ts
@@ -3,6 +3,7 @@ import { type FormFields } from "../../type";
 
 export function modelStore(formValue: FormFields): Prisma.WorkUnitCreateInput {
   return {
+    code: formValue.code,
     name: formValue.name,
     description: formValue.description,
     status: formValue.status,
