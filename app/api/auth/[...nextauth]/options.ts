aa496f071e768fe66b28e17d9d6f32cff854f103
rionalab <37329349+rionalab@users.noreply.github.com>
2024-02-17 08:13:08 +0700

    config next auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
+
+import { type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Input Username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Input Password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "11", name: "dave", password: "dave" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+};
