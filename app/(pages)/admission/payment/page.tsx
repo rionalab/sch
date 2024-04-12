0b76b3708784881b74ae5b5952f0c31a817e0a62
rionalab <rionalab@gmail.com>
2024-04-12 15:56:37 +0700

    develop admission payment page

--- app/(pages)/admission/payment/page.tsx
+++ app/(pages)/admission/payment/page.tsx
@@ -2,36 +2,99 @@
 
 import React from "react";
 import { Anchor, Row, Col } from 'antd';
+import SimpleBar from "simplebar-react";
+import styles from "./styles.module.scss";
 
 function Page() {
-  return <div style={{ background: "red"}}>
+  return <div className={styles.container}>
 
 <Row>
-    <Col span={6}>
+    <Col span={6} className={styles.anchor}>
     <Anchor
         items={[
           {
             key: 'part-1',
             href: '#part-1',
-            title: 'Part 1',
+            title: 'Payment Guideline',
           },
           {
             key: 'part-2',
             href: '#part-2',
-            title: 'Part 2',
+            title: 'Parents Declaration',
           },
           {
             key: 'part-3',
             href: '#part-3',
-            title: 'Part 3',
+            title: 'Site Policy',
           },
         ]}
       />
     </Col>
     
     <Col span={18} >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum labore laboriosam totam consequuntur, quidem necessitatibus facere nostrum fuga. Autem in dolorem explicabo doloribus eos quam eveniet amet alias aliquid at!
+      <SimpleBar style={{ maxHeight: 500}}>
+
+        <div id="part-1">
+          <h3>Payment Guideline</h3>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa commodi quis provident dolore, accusamus assumenda ullam facilis veniam nisi vero numquam. Ipsa pariatur repellat non perferendis harum expedita, saepe cumque.
+          </p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minima aspernatur aliquid quae hic nam deserunt similique officiis reprehenderit laudantium nostrum quisquam placeat, error quos facilis temporibus neque perspiciatis at?</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis error quam voluptate sunt adipisci dolore ipsum, suscipit veritatis deleniti quos, esse ea a nesciunt vero aspernatur pariatur voluptatibus ullam?
+
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam quasi facere consequuntur quia vitae! Sequi quas, animi dolor quod quae reiciendis qui ipsa quis consequatur velit harum at architecto?
+          </p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt necessitatibus aperiam alias odio explicabo ex molestiae ipsam repudiandae laborum cum aliquid aspernatur quaerat, distinctio amet. Dolore in optio facere asperiores.</p>
+
+        </div>
+
+        <div id="part-2">
+          <h3>Parents Declaration</h3>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa commodi quis provident dolore, accusamus assumenda ullam facilis veniam nisi vero numquam. Ipsa pariatur repellat non perferendis harum expedita, saepe cumque.
+          </p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minima aspernatur aliquid quae hic nam deserunt similique officiis reprehenderit laudantium nostrum quisquam placeat, error quos facilis temporibus neque perspiciatis at?</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis error quam voluptate sunt adipisci dolore ipsum, suscipit veritatis deleniti quos, esse ea a nesciunt vero aspernatur pariatur voluptatibus ullam?
+
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam quasi facere consequuntur quia vitae! Sequi quas, animi dolor quod quae reiciendis qui ipsa quis consequatur velit harum at architecto?
+          </p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt necessitatibus aperiam alias odio explicabo ex molestiae ipsam repudiandae laborum cum aliquid aspernatur quaerat, distinctio amet. Dolore in optio facere asperiores.</p>
+
+        </div>
+
+        <div id="part-3">
+          <h3>Site Policy</h3>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa commodi quis provident dolore, accusamus assumenda ullam facilis veniam nisi vero numquam. Ipsa pariatur repellat non perferendis harum expedita, saepe cumque.
+          </p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minima aspernatur aliquid quae hic nam deserunt similique officiis reprehenderit laudantium nostrum quisquam placeat, error quos facilis temporibus neque perspiciatis at?</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit corporis error quam voluptate sunt adipisci dolore ipsum, suscipit veritatis deleniti quos, esse ea a nesciunt vero aspernatur pariatur voluptatibus ullam?
+
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam quasi facere consequuntur quia vitae! Sequi quas, animi dolor quod quae reiciendis qui ipsa quis consequatur velit harum at architecto?
+          </p>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt necessitatibus aperiam alias odio explicabo ex molestiae ipsam repudiandae laborum cum aliquid aspernatur quaerat, distinctio amet. Dolore in optio facere asperiores.</p>
+
+        </div>
+
+      </SimpleBar>
     </Col>
+
+
     </Row>
 
     
